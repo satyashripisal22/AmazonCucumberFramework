407338097abbc1e9dcf0bf8371ca3188dc9bbd85
Satyashri <satyashripisal22@gmail.com>
2025-01-26 20:16:41 +0530

    Admin Time Slot Validation

--- src/test/java/stepdefinition/HomelyfLoginStep.java
+++ src/test/java/stepdefinition/HomelyfLoginStep.java
@@ -1,5 +1,7 @@
 package stepdefinition;
 
+import static org.testng.Assert.assertTrue;
+
 import java.time.Duration;
 
 import org.openqa.selenium.By;
@@ -13,6 +15,7 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import pom.HomelyfAdminTimeSlotPage;
 import pom.HomelyfAdminUserPage;
 import pom.HomelyfLoginPage;
 
@@ -20,6 +23,7 @@ public class HomelyfLoginStep {
 	WebDriver driver;
 	HomelyfLoginPage hlp;
 	HomelyfAdminUserPage hlap;
+	HomelyfAdminTimeSlotPage timeslotpage;
 
 	@Given("User opens URL {string} on Chrome")
 	public void user_opens_url_on_chrome(String url) {
@@ -64,15 +68,65 @@ public class HomelyfLoginStep {
 	public void enter_email_address_in_search_field() throws InterruptedException {
 		hlap.enterEmailAdd("satyashripisal2209@gmail.com");
 		Thread.sleep(1000);
-	    
 	}
 
 	@Then("Display User on select Email Address")
 	public void display_user_on_select_email_address(){
 		System.out.println(driver.getTitle());
 		Assert.assertEquals(driver.getPageSource().contains("Home Lyf"), true);
+	}	
+	
+	//--------------------Time Slot Verification-------------------------------------
+	
+	@Given("logged in as an admin")
+	public void logged_in_as_an_admin() {
+		timeslotpage = new HomelyfAdminTimeSlotPage(driver);
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 	}
 
+	@And("navigate to timeslot page")
+	public void navigate_to_timeslot_page() {
+		timeslotpage.clickOnTimeSlotItem();
+		timeslotpage.getItemList();
+		Assert.assertEquals("Time slots", timeslotpage.getTimeSlotHeadingTxt());
+	}
 
-	
+	@When("view the time slots on page")
+	public void view_the_time_slots_on_page() {
+		Assert.assertEquals(timeslotpage.isTimeSlotsDesplayed("ID"), true);
+		Assert.assertEquals(timeslotpage.isTimeSlotsDesplayed("Start Time"), true);
+		Assert.assertEquals(timeslotpage.isTimeSlotsDesplayed("End Time"), true);
+	}
+
+	@Then("verify the id Start time end time colomn shows correct format")
+	public void verify_the_id_start_time_end_time_colomn_shows_correct_format() {
+		
+		String startTimeTxt = timeslotpage.getStarttimeTxt();
+		String endTimeTxt = timeslotpage.getEndtimeTxt();
+		System.out.println("---> starttime: "+startTimeTxt);
+		System.out.println("---> endtime: "+endTimeTxt);
+		assertTrue(timeslotpage.isTimeFormatCorrect(startTimeTxt));
+		assertTrue(timeslotpage.isTimeFormatCorrect(endTimeTxt));
+	}
+
+	@When("click on kebab menu")
+	public void click_on_kebab_menu() {
+		boolean actual = timeslotpage.clicktimeslotdetailsButton();
+		assertTrue(actual);
+	}
+
+	@And("click on preview option")
+	public void click_on_preview_option() {
+		String previewHeading = timeslotpage.clickOnPreview();
+		Assert.assertEquals(previewHeading,"Time slot details");
+	}
+
+	@Then("verify the start and end time showing correct")
+	public void verify_the_start_and_end_time_showing_correct() {
+	   String previewStartTime = timeslotpage.getPreviewStartTime();
+	   String previewEndTime = timeslotpage.getPreviewEndTime();
+	   
+	   Assert.assertEquals(timeslotpage.isTimeFormatCorrect(previewStartTime), true);
+	   Assert.assertEquals(timeslotpage.isTimeFormatCorrect(previewEndTime), true);
+	}
 }
